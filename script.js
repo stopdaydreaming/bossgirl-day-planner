6d27ab5832a1714c7059a5d7a82930386559241c
stopdaydreaming <hello@stopdaydreaming.com>
2020-12-16 18:51:40 -0500

    cleaned up code

--- script.js
+++ script.js
@@ -17,7 +17,7 @@
 var todaysDate = $('#currentDay');
 todaysDate.text(moment().format('dddd, MMMM, Do YYYY'));
 
-var currTime = (moment().format('HH'));
+var currHour = (moment().format('HH'));
 
 var timeOfDay = ['9', '10', '11', '12', '13', '14', '15', '16', '17'];
 
@@ -25,30 +25,53 @@ for (var i = 0; i < timeOfDay.length; i++ ){
     var row = $('<div>');
     row.addClass('row time-block');
     
-        var hour = $('<div>');
-        hour.text(timeOfDay[i]);
-        hour.addClass('hour col-1');
-        row.append(hour);
+    var hour = $('<div>');
+    hour.text(timeOfDay[i]);
+    // var h = timeOfDay[i];
 
-        var textArea = $('<textarea>'); 
-        textArea.addClass('past description col-10');
-        row.append(textArea);
+    hour.addClass('hour col-1 mt-1');
+    row.append(hour);
 
-        var button = $('<button>');
-        button.append('<i class="far fa-save fa-2x"></i>');
-        button.addClass('saveBtn col-1');
-        button.on('click', saveEvent);
-        row.append(button);
+    var textArea = $('<textarea>'); 
+    textArea.addClass('past description col-10');
+    row.append(textArea);
+
+    var button = $('<button>');
+    button.append('<i class="far fa-save fa-2x"></i>');
+    button.addClass('saveBtn col-1');
+    button.on('click', saveEvent);
+    row.append(button);
     
     $('.container').append(row);
 }
 
 function saveEvent() {
-    var hour = $(this).siblings(".hour").text();
-    var textArea = $(this).siblings(".description").val();
+    var hour = $(this)
+        .siblings(".hour")
+        .text();
+
+    var textArea = $(this)
+        .siblings(".description")
+        .val();
+
     localStorage.setItem(hour, textArea);
+    colorCalendar();
 }
 
+function colorCalendar() {
+    console.log("hour being passed: ");
+    // console.log("current hour: " + currHour);
+
+    // if(h >= currHour) {
+    //     console.log("big")
+    // }
+    // else if (h == currHour) {
+    //     console.log("equal")
+    // }
+    // else if (h <= currHour) {
+    //     console.log("small")
+    // }
+}
 
 
 
