a1681a8297d4a351708f0fbd802478c75eeccd98
stopdaydreaming <hello@stopdaydreaming.com>
2020-12-17 15:15:08 -0500

    added jquery ready

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,55 @@
-var todaysDate = $("#currentDay");
-todaysDate.text(moment().format("dddd, MMMM, Do YYYY"));
+$(document).ready(function() {
 
-var currHour = moment().hours();
+    var todaysDate = $("#currentDay");
+    todaysDate.text(moment().format("dddd, MMMM, Do YYYY"));
 
-var timeOfDay = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+    var currHour = moment().hours();
 
-for (var i = 0; i < timeOfDay.length; i++) {
-  var row = $("<div>");
-  row.addClass("row time-block");
-  var hour = $("<div>");
-  var hourDisplay = moment()
-    .set("hour", timeOfDay[i])
-    .format("hA");
-  hour.text(hourDisplay);
+    var timeOfDay = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-  hour.addClass("hour col-1 mt-1");
-  row.append(hour);
+    for (var i = 0; i < timeOfDay.length; i++) {
+    var row = $("<div>");
+    row.addClass("row time-block");
+    var hour = $("<div>");
+    var hourDisplay = moment()
+        .set("hour", timeOfDay[i])
+        .format("hA");
+    hour.text(hourDisplay);
 
-  var textArea = $("<textarea>");
-  textArea.val(localStorage.getItem(hourDisplay));
-  textArea.addClass("past description col-10");
-  row.append(textArea);
+    hour.addClass("hour col-1 mt-1");
+    row.append(hour);
 
-  var button = $("<button>");
-  button.append('<i class="fas fa-save fa-2x"></i>');
-  button.addClass("saveBtn col-1");
-  button.on("click", saveEvent);
-  row.append(button);
+    var textArea = $("<textarea>");
+    textArea.val(localStorage.getItem(hourDisplay));
+    textArea.addClass("past description col-10");
+    row.append(textArea);
 
-  $(".container").append(row);
+    var button = $("<button>");
+    button.append('<i class="fas fa-save fa-md"></i>');
+    button.addClass("saveBtn col-1");
+    button.on("click", saveEvent);
+    row.append(button);
 
-  if (timeOfDay[i] === currHour) {
-    textArea.addClass("present");
-  } else if (timeOfDay[i] < currHour) {
-    textArea.addClass("past");
-  } else {
-    textArea.addClass("future");
-  }
-}
+    $(".container").append(row);
 
-function saveEvent() {
-  var hour = $(this)
-    .siblings(".hour")
-    .text();
+    if (timeOfDay[i] === currHour) {
+        textArea.addClass("present");
+    } else if (timeOfDay[i] < currHour) {
+        textArea.addClass("past");
+    } else {
+        textArea.addClass("future");
+    }
+    }
 
-  var textArea = $(this)
-    .siblings(".description")
-    .val();
+    function saveEvent() {
+    var hour = $(this)
+        .siblings(".hour")
+        .text();
 
-  localStorage.setItem(hour, textArea);
-}
+    var textArea = $(this)
+        .siblings(".description")
+        .val();
+
+    localStorage.setItem(hour, textArea);
+    }
+});
